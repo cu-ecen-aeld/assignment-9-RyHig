bc6e35f2bf6a5f48b8dd00421ef1aa49f75ccaa9
ryhig <ryanjeffhiggins@gmail.com>
2023-10-13 17:59:43 -0500

    fixed .gitmodules, and my code

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -7,19 +7,18 @@
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
 LDD_VERSION = c828c4841aacabce9958cba78d684f754e4602e3
-
-LDD_SITE = git@github.com:cu-ecen-aeld/assignments-7-and-later-RyHig.git
+LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-RyHig.git
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 
 LDD_MODULE_SUBDIRS = scull misc-modules
 
-define LDD_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin
-endef
+#define LDD_INSTALL_TARGET_CMDS
+#	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
+#	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
+#	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
+#	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin
+#endef
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
